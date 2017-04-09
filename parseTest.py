e799ca658faf5d4478db1f9312fcd4471ef7772b
ThreeSixes <actual@joshlucy.com>
2017-04-08 18:12:57 -0700

    Add GPLv3 text.

--- parseTest.py
+++ parseTest.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python
 
+"""
+This file is part of pyadm300 (https://github.com/ThreeSixes/pyadm300).
+
+pyadm300 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+pyadm300 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pyadm300.  If not, see <http://www.gnu.org/licenses/>. 
+"""
+
+
 import adm300parse
 from pprint import pprint
 
