e494cfc615855cc30d8642087df2480bdea4d3ed
ThreeSixes <actual@joshlucy.com>
2017-04-10 12:25:02 -0700

    Text update for license area.

--- parseTest.py
+++ parseTest.py
@@ -14,7 +14,10 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with pyadm300.  If not, see <http://www.gnu.org/licenses/>. 
+along with pyadm300.  If not, see <http://www.gnu.org/licenses/>.
+
+This file demonstrates how you can parse data sentences from an
+ADM-300
 """
 
 
